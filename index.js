17c1b48caf86b6f15550cbceb0c6900dc5a5617f
Tomasz Libich <t.libich@gmail.com>
2017-11-28 14:02:47 +0000

    Dev update

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 const core = require('alb3rt-core'),
     mongodb = require('mongodb'),
+    CONFIG = core.config,
     logger = core.logger,
     FILE_ID = 'index',
-    ENV = process.env;
+    ENV = process.env,
+    dbAddress = ENV.MONGODB_ADDRESS,
+    dbUser = ENV.MONGODB_USER,
+    dbPassword = ENV.MONGODB_PASSWORD;
 
 module.exports = new class Alb3rtDB {
     constructor() {
-        this.mongoClient = mongodb.MongoClient;
-        this.dbUrl = `mongodb://${ENV.MONGODB_USER}:${ENV.MONGODB_PASSWORD}@${ENV.MONGODB_ADDRESS}`;
-        this.testDbConnection();
+        this.init();
+    }
+
+    init() {
+        if (this.hasAllDbParams()) {
+            this.setupDbDetails();
+            this.testDbConnection();
+        } else {
+            logger.error(FILE_ID, 'Missing required DB params, aborting setup...');
+        }
+    }
+
+    setupDbDetails() {
+        CONFIG.DB = Object.assing({}, CONFIG.DB, {
+            CLIENT: mongodb.MongoClient,
+            URL: `mongodb://${dbUser}:${dbPassword}@${dbAddress}`
+        });
+    }
+
+    hasAllDbParams() {
+        return [
+            dbAddress,
+            dbUser,
+            dbPassword
+        ].filter(entry => entry === undefined).length === 0;
     }
 
     testDbConnection() {
         logger.log(FILE_ID, 'Attempting to establish connection with db ...');
-        this.mongoClient.connect(this.dbUrl, (error, db) => {
+        CONFIG.DB.CLIENT.connect(CONFIG.DB.URL, (error, db) => {
             if (error) {
                 logger.error(FILE_ID, `Database connection error: ${error}`);
                 return;
